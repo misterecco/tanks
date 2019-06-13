e10fef5d4ae8ef408658644efde0ae57536308a3
Kasia Kowalska <kasia.kowalska36@gmail.com>
2019-06-14 00:28:39 +0200

    Generate random board

--- src/Board.hs
+++ src/Board.hs
@@ -1,12 +1,15 @@
-#!/usr/bin/env stack
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Board where
 
 import Data.Array
 import Data.Serialize
-import GHC.Generics (Generic)
+import Data.Binary
+import Data.Bits
+import GHC.Generics (Rep, Generic)
 
 data Field =
       Bricks
@@ -16,9 +19,28 @@ data Field =
     | Empty
     deriving (Generic, Show)
     
-instance Serialize Field
+instance Binary Field
 
 type Board = Array (Int, Int) Field
 
+x_coeff :: Int
+x_coeff = 17
+
+y_coeff :: Int
+y_coeff = 67
+
+randomField :: Int -> Int -> Field
+randomField i j =
+    case mod (xor (i * 1000 + j) 8475845) 5 of
+        0 -> Bricks
+        1 -> Forest
+        2 -> Stone
+        3 -> Ice
+        _ -> Empty
+        
+
 getBoard :: Int -> Int -> Board
 getBoard n m = array ((0,0),(n-1,m-1)) (concat [ [ ((i, j), Empty) | j <- [0..n-1] ] | i <- [0..m-1]] )
+
+randomBoard :: Int -> Int -> Board
+randomBoard n m = array ((0,0),(n-1,m-1)) (concat [ [ ((i, j), randomField i j) | j <- [0..n-1] ] | i <- [0..m-1]] )
